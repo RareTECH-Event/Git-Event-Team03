fec7a5cfe18495e9f889affefed00b40c31b7a99
sathi-suke <plus22.sathisuke.22@icloud.com>
2026-04-23 22:24:22 +0900

    選択肢３を編集

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
         print("選択してください：")
         print("1: 赤")
         print("2: 青")
-        print("3: 選択肢3")
+        print("3: 黄色")
         print("q: 終了")
 
         choice = input("> ")
@@ -13,7 +13,7 @@ def main():
         elif choice == "2":
             print("青が選ばれました。")
         elif choice == "3":
-            print("選択肢3が選ばれました。")
+            print("黄色が選ばれました。")
         elif choice == "q":
             print("プログラムを終了します。")
             break
